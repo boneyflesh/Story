e0b2c5264d6f8193114cb8d9db71d428b752e3a4
boneyflesh <aarorng@yahoo.com>
2026-07-08 07:17:42 +0800

    V3 with interacting nexr buttons

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+window.onload = function() {
+    document.getElementById("scene1").style.display = "block";
+};
+
+document.querySelectorAll(".nextBtn").forEach(button => {
+    button.addEventListener("click", function() {
+        const targetId = this.getAttribute("data-target");
+        document.getElementById(targetId).style.display = "block";
+        this.style.display = "none"
+    })
+});
+
 document.getElementById("revealBtn").addEventListener("click", function() {
     document.getElementById("photoReveal").style.display = "block";
     this.style.display = "none"; //hide button after click
