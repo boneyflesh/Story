dffecaefe869579b57f7c9c9f3d1e926238f63d8
boneyflesh <aarorng@yahoo.com>
2026-07-07 19:33:29 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("revealBtn").addEventListener("click", function() {
+    document.getElementById("photoReveal").style.display = "block";
+    this.style.display = "none"; //hide button after click
+});
